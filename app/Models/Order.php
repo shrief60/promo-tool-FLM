26c8ec00f6b2f36a39aac2e7aee3deb6191f8cb4
shrieft96 <sherif.mohamed@tagaddod.com>
2024-05-15 12:15:54 +0300

    add ORM relationships to models

---
+++ app/Models/Order.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'promotion_id',
+        'total_price',
+        'discount_amount',
+        'final_price',
+        'status',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function promotion()
+    {
+        return $this->belongsTo(Promotion::class);
+    }
+}
---
+++ app/Models/Promotion.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Promotion extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'promo_code',
+        'type',
+        'title',
+        'desc',
+        'reference_value',
+        'is_expired',
+        'user_segment',
+        'expiry_date',
+        'max_usage_times',
+        'max_usage_times_per_user',
+    ];
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'promotion_users')->withPivot('available_usage_times');
+    }
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
+}
