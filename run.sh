03d99061999823b471b2382a3cd0dd9c2527c26a
shrieft96 <sherif.mohamed@tagaddod.com>
2024-05-16 11:31:20 +0300

    image dockerization

---
+++ run.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+mkdir -p /var/www/html/storage
+mkdir -p /var/www/html/storage/app
+mkdir -p /var/www/html/storage/logs
+mkdir -p /var/www/html/storage/framework
+mkdir -p /var/www/html/storage/framework/cache
+mkdir -p /var/www/html/storage/framework/sessions
+mkdir -p /var/www/html/storage/framework/views
+mkdir -p /var/www/html/storage/framework/logs
+
+chmod 777 /var/www/html/storage -R
+chmod 777 /var/www/html/storage/app -R
+chmod 777 /var/www/html/storage/logs -R
+chmod 777 /var/www/html/storage/framework -R
+chmod 777 /var/www/html/storage/framework/cache -R
+chmod 777 /var/www/html/storage/framework/sessions -R
+chmod 777 /var/www/html/storage/framework/views -R
+chmod 777 /var/www/html/storage/framework/logs -R
+chmod 777 /var/www/html/bootstrap/cache -R
+
+chown www-data:www-data /var/www/html/storage/logs/* -R
+chown www-data:www-data /var/www/html/storage/logs -R
+
+php artisan migrate
+php artisan cache:clear
+php artisan config:clear
+php artisan route:clear
